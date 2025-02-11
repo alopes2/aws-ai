60c480730a8b356a04626bf9da6b8f141ae85341
Andre <andrevitorlopes@gmail.com>
2025-02-11 21:10:37 +0100

    Optmize lambda

--- transcribe/app/src/main.py
+++ transcribe/app/src/main.py
@@ -13,33 +13,35 @@ JOB_ROLE_ARN = os.environ["JOB_ROLE_ARN"]
 transcribe_client = boto3.client("transcribe")
 
 def handler(event, context):
-  for record in event["Records"]:
-    bucket = record['s3']['bucket']['name']
-    key = unquote_plus(record['s3']['object']['key'])
-    fileInput = f"s3://{bucket}/{key}"
+    for record in event["Records"]:
+        bucket = record['s3']['bucket']['name']
+        key = unquote_plus(record['s3']['object']['key'])
+        fileInput = f"s3://{bucket}/{key}"
 
-    job_name = key.replace("/", "_") + str(time.time()).replace(".","")
-    
-    media_format = key.split(".")[-1]
+        job_name = key.replace("/", "_") + str(time.time()).replace(".","")
 
-    try:
-        job_args = {
-            "TranscriptionJobName": job_name,
-            "Media": {"MediaFileUri": fileInput},
-            "MediaFormat": media_format,
-            "LanguageCode": "en-US",
-            "OutputBucketName": bucket,
-            "OutputKey": f"transcription/{job_name}.txt",
-            "JobExecutionSettings": {
-               "DataAccessRoleArn": JOB_ROLE_ARN
+        media_format = key.split(".")[-1]
+
+        try:
+            job_args = {
+                "TranscriptionJobName": job_name,
+                "Media": {"MediaFileUri": fileInput},
+                "MediaFormat": media_format,
+                "LanguageCode": "en-US",
+                "OutputBucketName": bucket,
+                "OutputKey": f"transcription/{job_name}.json",
+                "JobExecutionSettings": {
+                    "DataAccessRoleArn": JOB_ROLE_ARN
+                }
             }
-        }
+            logger.info("Started transcription job %s.", job_name)
 
-        response = transcribe_client.start_transcription_job(**job_args)
-        job = response["TranscriptionJob"]
-        logger.info("Started transcription job %s.", job_name)
-    except ClientError:
-        logger.exception("Couldn't start transcription job %s.", job_name)
-        raise
-    else:
-        return job
+            response = transcribe_client.start_transcription_job(**job_args)
+            job = response["TranscriptionJob"]
+            logger.info("Finished job %s. Data %s", job_name, job)
+        except ClientError:
+            logger.exception("Couldn't start transcription job %s.", job_name)
+            raise
+    
+    logger.info("Finshed with S3 records processing")
+
