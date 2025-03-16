39e67f782e4b07ef3f0176377a9fb1ee4a59475e
Andre Lopes <andrevitorlopes@gmail.com>
2025-03-16 16:14:30 +0100

    Fix transcribe policies

--- transcribe/iac/policies.tf
+++ transcribe/iac/policies.tf
@@ -62,7 +62,7 @@ data "aws_iam_policy_document" "job_policies" {
 
     actions = ["s3:GetObject"]
 
-    resources = ["${aws_s3_object.audio.arn}*"]
+    resources = ["${aws_s3_object.media.arn}*"]
   }
   statement {
     effect = "Allow"
