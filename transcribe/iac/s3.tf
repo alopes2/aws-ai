f5ecd81b2aaa01fa17ea7fe8073fda473e5357cd
Andre <andrevitorlopes@gmail.com>
2025-03-17 17:14:47 +0100

    Fix path to source

--- transcribe/iac/s3.tf
+++ transcribe/iac/s3.tf
@@ -55,29 +55,29 @@ resource "aws_s3_object" "tune_data" {
 resource "aws_s3_object" "nintendo_switch" {
   bucket      = aws_s3_bucket.bucket.id
   key         = "${aws_s3_object.training_data.key}NintendoSwitch.txt"
-  source      = "${path.module}/training_data/NintendoSwitch.txt"
-  source_hash = filemd5("${path.module}/training_data/NintendoSwitch.txt")
+  source      = "${path.module}/transcribe/training_data/NintendoSwitch.txt"
+  source_hash = filemd5("${path.module}/transcribe/training_data/NintendoSwitch.txt")
 }
 
 resource "aws_s3_object" "ps5" {
   bucket      = aws_s3_bucket.bucket.id
   key         = "${aws_s3_object.training_data.key}PlayStation5.txt"
-  source      = "${path.module}/training_data/PlayStation5.txt"
-  source_hash = filemd5("${path.module}/training_data/PlayStation5.txt")
+  source      = "${path.module}/transcribe/training_data/PlayStation5.txt"
+  source_hash = filemd5("${path.module}/transcribe/training_data/PlayStation5.txt")
 }
 
 resource "aws_s3_object" "xbox" {
   bucket      = aws_s3_bucket.bucket.id
   key         = "${aws_s3_object.training_data.key}XboxSeries.txt"
-  source      = "${path.module}/training_data/XboxSeries.txt"
-  source_hash = filemd5("${path.module}/training_data/XboxSeries.txt")
+  source      = "${path.module}/transcribe/training_data/XboxSeries.txt"
+  source_hash = filemd5("${path.module}/transcribe/training_data/XboxSeries.txt")
 }
 
 resource "aws_s3_object" "tune_data_file" {
   bucket      = aws_s3_bucket.bucket.id
   key         = "${aws_s3_object.tune_data.key}tune_data.txt"
-  source      = "${path.module}/tune_data/tune_data.txt"
-  source_hash = filemd5("${path.module}/tune_data/tune_data.txt")
+  source      = "${path.module}/transcribe/tune_data/tune_data.txt"
+  source_hash = filemd5("${path.module}/transcribe/tune_data/tune_data.txt")
 }
 
 resource "aws_lambda_permission" "allow_bucket" {
