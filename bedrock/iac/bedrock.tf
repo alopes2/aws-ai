96f4a3d2b9ca12cc0b7d0117e1cc051f3b45dc51
Andre <andrevitorlopes@gmail.com>
2025-03-28 17:41:07 +0100

    Use inference profile for Claude 3.7

--- bedrock/iac/bedrock.tf
+++ bedrock/iac/bedrock.tf
@@ -1,3 +1,7 @@
 data "aws_bedrock_foundation_model" "claude" {
   model_id = "anthropic.claude-3-7-sonnet-20250219-v1:0"
 }
+
+data "aws_bedrock_inference_profile" "claude" {
+  inference_profile_id = "eu.anthropic.claude-3-7-sonnet-20250219-v1:0"
+}
