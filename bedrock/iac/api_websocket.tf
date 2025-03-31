67c0f91b16e2d7c4412aca4ddb044280c03243f1
Andre <andrevitorlopes@gmail.com>
2025-03-31 21:57:36 +0200

    Fix resoruce names

--- bedrock/iac/api_websocket.tf
+++ bedrock/iac/api_websocket.tf
@@ -15,19 +15,19 @@ resource "aws_apigatewayv2_stage" "stage" {
   auto_deploy = true
 }
 
-resource "aws_apigatewayv2_route" "example" {
+resource "aws_apigatewayv2_route" "default" {
   api_id    = aws_apigatewayv2_api.api.id
   route_key = "$default"
   target    = "integrations/${aws_apigatewayv2_integration.default.id}"
 }
 
-resource "aws_apigatewayv2_route" "example" {
+resource "aws_apigatewayv2_route" "connect" {
   api_id    = aws_apigatewayv2_api.api.id
   route_key = "$connect"
   target    = "integrations/${aws_apigatewayv2_integration.connections.id}"
 }
 
-resource "aws_apigatewayv2_route" "example" {
+resource "aws_apigatewayv2_route" "disconnect" {
   api_id    = aws_apigatewayv2_api.api.id
   route_key = "$disconnect"
   target    = "integrations/${aws_apigatewayv2_integration.connections.id}"
