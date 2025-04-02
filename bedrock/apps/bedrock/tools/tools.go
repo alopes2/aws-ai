7108317e0aced2e0bfef863b9c1796845a96ab7d
Andre <andrevitorlopes@gmail.com>
2025-04-02 21:42:00 +0200

    Try with struct

--- bedrock/apps/bedrock/tools/tools.go
+++ bedrock/apps/bedrock/tools/tools.go
@@ -4,19 +4,29 @@ func GetWeather(city string) string {
 	return "It is sunny in " + city
 }
 
-func GetWeatherToolSchema() map[string]map[string]any {
-	return map[string]map[string]any{
-		"json": {
-			"type": "object",
-			"properties": map[string]any{
+func GetWeatherToolSchema() ToolSchema {
+	return ToolSchema{
+		Json: ToolJsonSchema{
+			Type: "object",
+			Properties: map[string]any{
 				"city": map[string]string{
 					"type":        "string",
 					"description": "The city to get the current weather for. Example cities are Berlin, New York, Paris",
 				},
 			},
-			"required": []string{
+			Required: []string{
 				"city",
 			},
 		},
 	}
+}
+
+type ToolSchema struct {
+	Json ToolJsonSchema `json:"json"`
+}
+
+type ToolJsonSchema struct {
+	Type       string         `json:"type"`
+	Required   []string       `json:"required"`
+	Properties map[string]any `json:"properties"`
 }
