e51d1a7bf959f577ef6393dc86a916bc129fdab2
Andre <andrevitorlopes@gmail.com>
2025-04-03 10:36:58 +0200

    Remove higher level JSON object

--- bedrock/apps/bedrock/tools/tools.go
+++ bedrock/apps/bedrock/tools/tools.go
@@ -4,25 +4,19 @@ func GetWeather(city string) string {
 	return "It is sunny in " + city
 }
 
-func GetWeatherToolSchema() ToolSchema {
-	return ToolSchema{
-		Json: ToolJsonSchema{
-			Type: "object",
-			Properties: ToolJsonSchemaProperties{
-				"city": ToolJsonSchemaProperty{
-					Type:        "string",
-					Description: "The city to get the current weather for. Example cities are Berlin, New York, Paris",
-				},
-			},
-			Required: []string{
-				"city",
+func GetWeatherToolSchema() ToolJsonSchema {
+	return ToolJsonSchema{
+		Type: "object",
+		Properties: ToolJsonSchemaProperties{
+			"city": ToolJsonSchemaProperty{
+				Type:        "string",
+				Description: "The city to get the current weather for. Example cities are Berlin, New York, Paris",
 			},
 		},
+		Required: []string{
+			"city",
+		},
 	}
-}
-
-type ToolSchema struct {
-	Json ToolJsonSchema `json:"json"`
 }
 
 type ToolJsonSchema struct {
